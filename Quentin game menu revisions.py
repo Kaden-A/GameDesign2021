0f82028b7e32699889e5a3ac3e4119abce3f1844
Kaden <alibhaik24@greenhill.org>
2021-06-09 10:32:42 -0500

    Added 6 strings

--- Quentin game menu revisions.py
+++ Quentin game menu revisions.py
@@ -6,14 +6,17 @@
 #to declare a function we must use the keyword def give a name
 
 #Global variables can be used anywhere in the program
-l1="**************************"
-l2="*       My game          *"
-l3="*                        *"
-l4="*    1 - Level1          *"
-l5="*    2 - Level2          *"
-l6="*    3 - Scores          *"
-l7="*    4 - Exit Game       *"
-l8="**************************"
+l1= "***************************************"
+l2= "*           Strings Game              *"
+l3= "*                                     *"
+l4= "*    1 - Capitalize                   *"
+l5= "*    2 - Uppercase                    *"
+l6= "*    3 - Lowercase                    *"
+l7= "*    4 - Find index of character      *"
+l8= "*    5 - Split                        *"
+l9= "*    6 - Translate                    *"
+l10="*    7 - Exit Game                    *"
+l11="***************************************"
 x=1  #global variable
 def menu():
     print(l1)
@@ -24,7 +27,10 @@ def menu():
     print(l6)
     print(l7)
     print(l8)
-    print("please enter your selection from 1 to 4")
+    print(l9)
+    print(l10)
+    print(l11)
+    print("please enter your selection from 1 to 7")
     inputNumber = input()
     x = int(inputNumber)
     return x
@@ -37,16 +43,49 @@ def score():
     print("*    3 - ???- 745        *")
     print(l3)
     print(l8)
-while x !=4:  
+def pause():
+    print("Press enter to continue")
+    input()
+
+while x !=7:  #loop is conditioned to an event
     x=menu()
     if(x==1):        #if statement are selection or branching
-        print("Level 1 Chosen")
+        print("Please enter a phrase in lower case")
+        answer=input()   #input is a function
+        print(answer.capitalize()) #is a method of strings and you have referred with "."
+        answer=answer.capitalize() #update variable to the new changed if i dont need original value
+        pause()
+        #let the user stay in the level and reuse it many times until they want to go back to the menu
 
     if(x==2):
-        print("Level 2 Chosen")
+        print("Input a phrase with lower case letters")
+        answer=input()
+        print(answer.upper())
+        answer=answer.upper()
+        pause()
 
     if(x==3):
-        score()
-
-
-print("Thank you for playing")
+        print("Please enter a phrase in upper case")
+        answer=input()
+        print(answer.lower())
+        pause()
+    
+    if(x==4):
+        print("Enter the value you want indexed")
+        answer=input()
+        print(answer.index())
+        pause()
+    
+    if(x==5):
+        print("Enter the string you want to be split")
+        answer=input()
+        print(answer.split())
+        pause()
+    if(x==6):
+        print("Input the phrase you would like to translate")
+        answer=input()
+        mydict= {83: 80}
+        print(answer.translate(mydict))
+        pause()
+    if(x==7):
+        print("Thank you for playing")
