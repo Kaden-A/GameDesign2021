d4cca36407cfab2dddc2a6237a87be8d55ebea98
Kaden <alibhaik24@greenhill.org>
2021-06-11 09:31:37 -0500

    Added tuples and random choices

--- ListsAndTuples.py
+++ ListsAndTuples.py
@@ -1,6 +1,10 @@
 #Kaden Alibhai 6/10/2021
 #We are learning about lists and tuples
 #Learn their functions and looping with lists
+
+#How to use a module or library by importing
+import random
+
 myFruit=["apples", "berries", "mangos", "bananas"]
 print(myFruit)
 for fruit in myFruit:
@@ -10,6 +14,13 @@ for fruit in myFruit: #for the length of your array
     print(fruit, end=" , ")
 print()
 counter=len(myFruit) #The length of the list is one more than the last index
+#finding a random number to be the index to select randint()
+indx= random.randint(0,counter-1)
+print("your lucky fruit is ", myFruit[indx])
+
+#random method choice()
+word=random.choice(myFruit)
+print("Your random fruit is ", word)
 
 for x in range(0,counter-1):
     print(myFruit[x], end=" , ")
@@ -24,3 +35,14 @@ myFruit.insert(0,"kiwi")
 myFruit.insert(2,"papaya")
 myFruit.append("beets")
 print(myFruit)
+
+fruity=("apple", "pears", "banana") #tuple
+print("tuple",fruity)
+temp= list (fruity) #temp is a list
+print("list",temp)
+temp.insert(1,"kiwi")
+fruity=tuple(temp)
+print("tuple modified", fruity)
+print("list modified", temp)
+for element in fruity:
+    print(element)
