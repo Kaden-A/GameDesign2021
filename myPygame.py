66fac20d3ad958378b90c2a81f52cb894624dcf7
Kaden <alibhaik24@greenhill.org>
2021-06-16 18:56:52 -0500

    Changed the screen color

--- myPygame.py
+++ myPygame.py
@@ -8,6 +8,9 @@ pygame.time.delay(100)
 WIDTH=600
 HEIGHT=400
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
+color= (0, 255, 255)
+screen.fill(color)
+pygame.display.flip()
 
 check=True
 while check:
