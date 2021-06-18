7b507f75432c7f6e720918ce41470a6661c126f3
Kaden <alibhaik24@greenhill.org>
2021-06-17 19:39:07 -0500

    square can move around with keys

--- myPygame.py
+++ myPygame.py
@@ -1,38 +1,56 @@
-import pygame
-import time
-import datetime
-import sys
-
+import time, sys, pygame
+print(sys.path)
 pygame.init() #Initialize the game
-pygame.time.delay(100) # delay in milliseconds
-WIDTH=600
-HEIGHT=400
+pygame.time.delay(100)  #delay in milliseconds
+WIDTH=500
+HEIGHT=600
+white=[255,255,255]
+purple=[200,0,190]
+green=[50,25,255]
+#create object to open window
+ 
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
-cyan= (0, 255, 255)
-red= (255,0,0)
-green=(50,25,255)
-white=(255,255,255)
-screen.fill(cyan)
-pygame.display.set_caption("Kaden's Game") #Change title
-pygame.display.flip() #command to do something
-
-check=True
+screen.fill(purple)
+pygame.display.set_caption('My Game') #Chanhe title onn the screen and you can also change icon
+pygame.display.update() #command to actually do something
+ 
+#you must always ....ALWAYS
+check = True
 x=10
 y=10
 rad=30
-hbox, wbox=20,20
-rect=pygame.Rect(x,y,hbox,wbox)
-
+hbox, wbox =20,20
+rect=pygame.Rect(x,y,hbox,wbox) #this creates a rectangle
+ 
 while check:
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
-            check=False
-    pygame.draw.rect(screen,(red), rect)
-    rect.x=40
-    rect.y=100
-    pygame.draw.rect(screen,(white), rect)
-    pygame.draw.circle(screen,(green),(x+300,y+200),rad,3)
+            check = False
+    speed=5
+    keyBoardKey=pygame.key.get_pressed()  #checking what key is pressed
+    if keyBoardKey[pygame.K_LEFT]:  #Moving left on x (-)
+        rect.x -=speed
+     
+    if keyBoardKey[pygame.K_RIGHT]:  #Moving right on x(+)
+        rect.x +=speed
+    
+    if keyBoardKey[pygame.K_UP]: #Moving up on y (-)
+        rect.y -=speed
+    if keyBoardKey[pygame.K_DOWN]: #Moving down on y (-)
+        rect.y +=speed
+    if keyBoardKey[pygame.K_s]:
+        rad -=speed
+    if keyBoardKey[pygame.K_f]:
+        rad += speed
+    if rect.x < 0: rect.x=0
+    if rect.x > WIDTH-wbox: rect.x = WIDTH-wbox
+    if rect.y < 0: rect.y=0
+    if rect.y > HEIGHT-hbox: rect.y = HEIGHT-hbox
+    if rad < 0: rad=1
+    if rad > WIDTH-x: rad = WIDTH-x
+    screen.fill(purple)
+    pygame.draw.rect(screen,(green),rect)
+    pygame.draw.circle(screen,(white),(x+300,y+200),rad,2)
     pygame.display.flip()
     pygame.time.delay(30)
-
 pygame.quit()
