99be4bdf58256f12bd21a628301b4384a97d66c0
Kaden <alibhaik24@greenhill.org>
2021-06-18 10:32:23 -0500

    added jump and images

--- myPygame.py
+++ myPygame.py
@@ -1,14 +1,18 @@
 import time, sys, pygame
+from pygame.draw import circle
 print(sys.path)
 pygame.init() #Initialize the game
 pygame.time.delay(100)  #delay in milliseconds
-WIDTH=500
-HEIGHT=600
+WIDTH=600
+HEIGHT=450
+bg=pygame.image.load("Images/basketball.jpg")
+flame=pygame.image.load("Images\Flames.jpg")
 white=[255,255,255]
 cyan=[0,255,255]
 red=[255,0,0]
 green=[50,25,255]
 blue=[0,0,255]
+purple=[200,0,190]
 #create object to open window
  
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
@@ -23,7 +27,9 @@ y=10
 rad=30
 hbox, wbox =20,20
 rect=pygame.Rect(x,y,hbox,wbox) #this creates a rectangle
- 
+rect1=pygame.Rect(x+20,y+50,hbox,wbox)
+JumpCheck=False
+jump=10
 while check:
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
@@ -32,25 +38,61 @@ while check:
     keyBoardKey=pygame.key.get_pressed()  #checking what key is pressed
     if keyBoardKey[pygame.K_LEFT]:  #Moving left on x (-)
         rect.x -=speed
-     
     if keyBoardKey[pygame.K_RIGHT]:  #Moving right on x(+)
         rect.x +=speed
-    
-    if keyBoardKey[pygame.K_UP]: #Moving up on y (-)
-        rect.y -=speed
-    if keyBoardKey[pygame.K_DOWN]: #Moving down on y (-)
-        rect.y +=speed
+#is to check if we are going to jump
+    if keyBoardKey[pygame.K_SPACE]:
+        JumpCheck=True
+    else:
+        JumpCheck=False
+    if not JumpCheck:
+        if keyBoardKey[pygame.K_UP]: #Moving up on y (-)
+            rect.y -=speed
+        if keyBoardKey[pygame.K_DOWN]: #Moving down on y (-)
+            rect.y +=speed
+    else:
+        if jump >=-10:
+            rect.y-=(jump*abs(jump))/2
+            jump-=1
+        else:
+            jump=10
+            JumpCheck=False
+
     if keyBoardKey[pygame.K_s]:
         rad -=speed
     if keyBoardKey[pygame.K_f]:
         rad += speed
+    
+    if keyBoardKey[pygame.K_j]:  #Moving left on x (-)
+        rect1.x -=speed
+    if keyBoardKey[pygame.K_l]:  #Moving right on x(+)
+        rect1.x +=speed
+    if keyBoardKey[pygame.K_i]: #Moving up on y (-)
+        rect1.y -=speed
+    if keyBoardKey[pygame.K_k]: #Moving down on y (-)
+        rect1.y +=speed
+
     if rect.x < 0: rect.x=0
     if rect.x > WIDTH-wbox: rect.x = WIDTH-wbox
     if rect.y < 0: rect.y=0
     if rect.y > HEIGHT-hbox: rect.y = HEIGHT-hbox
+    
     if rad < 0: rad=1
     if rad > WIDTH-x: rad = WIDTH-x
+    
+    if rect1.x < 0: rect1.x=0
+    if rect1.x > WIDTH-wbox: rect1.x = WIDTH-wbox
+    if rect1.y < 0: rect1.y=0
+    if rect1.y > HEIGHT-hbox: rect1.y = HEIGHT-hbox
+    
+    if rect.colliderect(rect1):
+        rect.x-=3
+        rect1.x+=3
+
     screen.fill(cyan)
+    screen.blit(bg,(0,0))
+    screen.blit(flame, (0,350))
+    pygame.draw.rect(screen,(green),rect1)
     pygame.draw.rect(screen,(red),rect)
     pygame.draw.circle(screen,(blue),(x+300,y+200),rad,2)
     pygame.display.flip()
