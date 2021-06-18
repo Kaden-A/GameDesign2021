5567541dfda1945d9130569a63df17fb65f277df
Kaden <alibhaik24@greenhill.org>
2021-06-17 19:41:45 -0500

    changed color of shapes and screen

--- myPygame.py
+++ myPygame.py
@@ -5,12 +5,14 @@ pygame.time.delay(100)  #delay in milliseconds
 WIDTH=500
 HEIGHT=600
 white=[255,255,255]
-purple=[200,0,190]
+cyan=[0,255,255]
+red=[255,0,0]
 green=[50,25,255]
+blue=[0,0,255]
 #create object to open window
  
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
-screen.fill(purple)
+screen.fill(cyan)
 pygame.display.set_caption('My Game') #Chanhe title onn the screen and you can also change icon
 pygame.display.update() #command to actually do something
  
@@ -48,9 +50,9 @@ while check:
     if rect.y > HEIGHT-hbox: rect.y = HEIGHT-hbox
     if rad < 0: rad=1
     if rad > WIDTH-x: rad = WIDTH-x
-    screen.fill(purple)
-    pygame.draw.rect(screen,(green),rect)
-    pygame.draw.circle(screen,(white),(x+300,y+200),rad,2)
+    screen.fill(cyan)
+    pygame.draw.rect(screen,(red),rect)
+    pygame.draw.circle(screen,(blue),(x+300,y+200),rad,2)
     pygame.display.flip()
     pygame.time.delay(30)
 pygame.quit()
