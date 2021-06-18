cbf44d5430beeec5322c8fa8084206b625276c8e
Kaden <alibhaik24@greenhill.org>
2021-06-17 19:37:52 -0500

    drew a rectangle and circle

--- myPygame.py
+++ myPygame.py
@@ -3,18 +3,36 @@ import time
 import datetime
 import sys
 
-pygame.init()
-pygame.time.delay(100)
+pygame.init() #Initialize the game
+pygame.time.delay(100) # delay in milliseconds
 WIDTH=600
 HEIGHT=400
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
-color= (0, 255, 255)
-screen.fill(color)
-pygame.display.flip()
+cyan= (0, 255, 255)
+red= (255,0,0)
+green=(50,25,255)
+white=(255,255,255)
+screen.fill(cyan)
+pygame.display.set_caption("Kaden's Game") #Change title
+pygame.display.flip() #command to do something
 
 check=True
+x=10
+y=10
+rad=30
+hbox, wbox=20,20
+rect=pygame.Rect(x,y,hbox,wbox)
+
 while check:
     for event in pygame.event.get():
         if event.type== pygame.QUIT:
             check=False
+    pygame.draw.rect(screen,(red), rect)
+    rect.x=40
+    rect.y=100
+    pygame.draw.rect(screen,(white), rect)
+    pygame.draw.circle(screen,(green),(x+300,y+200),rad,3)
+    pygame.display.flip()
+    pygame.time.delay(30)
+
 pygame.quit()
