5e3ba0073709f70b9f9cf2ab4e1192b5a1d779b6
Kaden <alibhaik24@greenhill.org>
2021-06-22 10:33:03 -0500

    finished game

--- HangmanPyg.py
+++ HangmanPyg.py
@@ -1,94 +1,144 @@
-# Kaden Alibhai
-# Create a Hangman version of the wordgame
-# Useing images, lists, fonts, rendering
+# Maria Suarez  
+# Create a Hanggman version of the game:  
+# Use images in a list, Use Fonts, render them  
 
-import pygame, math, random, sys, time, os
+import os, sys, time, pygame, random, math
 
-os.system('cls')
-pygame.init()
+from pygame.constants import K_1  
+os.system('cls')  
+pygame.init()  
 
-#create our screen or window
+#Create my display screen  
+WIDTH = 800  # uppercase because it behaves as constant  
+HEIGHT = 500   
+screen = pygame.display.set_mode((WIDTH,HEIGHT))  
+pygame.display.set_caption("Hangman GAME!")   
 
-WIDTH=800
-HEIGHT=500
-screen=pygame.display.set_mode((WIDTH,HEIGHT))
-pygame.display.set_caption("Hangman Game!")
-#Define colors
-WHITE=[255,255,255]
-BLACK=[0,0,0]
-screen.fill(WHITE)
-pygame.display.update()
+# Define Colors  
+WHITE = [255,255,255]  
+BLACK = [0,0,0]  
 
-#set up fonts
-TitleFont = pygame.font.SysFont("comicsans", 70)
-WordFont= pygame.font.SysFont("comicsane", 50)
-def updateScreen(turns, displayWord):
-    screen.fill
-    screen.blit(images[turns], (80,100))
-    pygame.display.update
-    pygame.time.delay(500)
-    
-#Words list
-gameWords= ['lebron james','kevin durant','giannis antetokounmpo','kawhi leonard',
-'nikola jokic','damian lillard','james harden','jayson tatum','luka doncic',
-'anthony davis','stephen curry','joel embiid']
+# Word lists  
+gameWords= ['python','java','trackpad','computer','keyboard','geeks','laptop','headphones','charger','mouse','software','hardware']  
 
-#Load images
-images= []
-for i in range(7):
-    image=pygame.image.load("HangmanImages\hangman"+str(i)+".png")
-    images.append(image)
-    screen.blit(images[i], (100,100))
-    pygame.display.update()
-    pygame.time.delay(500)
+# load images to list  
+images = []  
+for i in range(7):  
+    image=pygame.image.load("HangmanImages\\hangman"+ str(i)+".png")  
+    images.append(image)  
+    # screen.blit(images[i], (100,100))  
+    # pygame.display.update()  
+    # pygame.time.delay(300)  
+  
+# Define Font objects  
+TitleFont= pygame.font.SysFont("comicsans", 70)  #set the type of font and the size   
+WordFont=pygame.font.SysFont("comicsans", 50)  
+LetterFont=pygame.font.SysFont("comicsans", 40) 
+ 
+#Define letters for rectangular buttons 
+A=65 
+Wbox=30 
+dist=10 
+letters=[]#an array of arrays  [[x,y, ltr, boolean]] 
+#DEfine where to start our drawing 26 letter, 13 letter in each line 
+startx= round((WIDTH - (Wbox + dist)*13) /2) #int function round 
+starty= 350 
+#load the letters into our double array 
+for i in range(26): 
+    x=startx +dist*2+((Wbox +dist)*(i%13)) 
+    y=starty+((i//13)*(dist + Wbox *2)) 
+    letters.append([x,y,chr(A+i), True]) 
 
-def updateWord(word, guesses, turns):  # function with a parameter to update word
-    displayWord =" "
+#Function to update the screen 
+def updateScreen(turns,displayWord):  
+    screen.fill(WHITE)  
+    title=TitleFont.render("Hangman",1, BLACK)  
+    centerTitle= WIDTH/2-title.get_width()/2  
+    screen.blit(title, (centerTitle,20))  
+    screen.blit(images[turns], (100,100))  
+    textW=WordFont.render(displayWord,1, BLACK)  
+    screen.blit(textW, (300,150))  
+    for letter in letters: 
+        x,y,ltr, see= letter 
+        if see: 
+            rect=pygame.Rect(x-Wbox/2,y-Wbox/2,Wbox,Wbox) 
+            pygame.draw.rect(screen, BLACK, rect, width=1)  
+            text=LetterFont.render(ltr,1,BLACK) 
+            screen.blit(text,(x -text.get_width()/2,y -text.get_height()/2)) 
+    pygame.display.update()  
+ 
+def updateWord(word, guesses):  # function with a parameter to update word  
+    displayWord=""  
+    for char in word:  
+        if char in guesses:  
+            displayWord += char+" "  
+        else:  
+            displayWord += "_ "  
+    return displayWord  
+ 
+def dis_message(message): 
+    screen.fill(WHITE) 
+    text =TitleFont.render(message,1,BLACK) 
+    screen.blit(text, (200,200)) 
+    pygame.display.update() 
+    pygame.time.delay(1)
+
+def cont():
     screen.fill(WHITE)
+    msg="Would you like to play again?"
+    msg2="Press 1 to play again, press 2 to exit"
+    text1=TitleFont.render(msg,1,BLACK)
+    text2= TitleFont.render(msg2,1,BLACK)
+    screen.blit(text1, (200,100))
+    screen.blit(text2, (200,200))
     pygame.display.update
-    for char in word:
-        if char in guesses:
-            print(char,end=' ')
-            displayWord += char+" "
-        else:
-            displayWord += "_ "
-            print('_', end =' ')
-    textW=WordFont.render(displayWord,1, BLACK)
-    screen.blit(textW,(350,150))
-    text=TitleFont.render("HANGMAN", 1, BLACK)
-    centerTitle=WIDTH/2-text.get_width()/2
-    screen.blit(text,(centerTitle,20))
-    screen.blit(images[turns], (80,100))
-    pygame.display.update()
+    keyBoardKey=pygame.key.get_pressed()
+    if keyBoardKey[pygame.K_1]:
+        print("Goodbye")
+    if keyBoardKey[pygame.K_2]:
+        pygame.quit
 
-check= True
-while check:
-    for event in pygame.event.get():
-        if event.type== pygame.QUIT:
-            check=False
-    word=random.choice(gameWords)
-    guesses=""
-    counter=len(word)
-    print(word)
-    print("The player's name contains",counter,"letters")
-    turns=0  #should we consider controlling this number when they miss
-    displayWord= updateWord(word, guesses, turns)
-    updateScreen(turns, displayWord)
-    while turns<7 and counter >0:
-        newGuess=input("\n\n Give me a letter ")
-        if newGuess not in guesses:
-            if newGuess not in word:
-                turns +=1    #       turns = turns -1
-                print("Wrong! You have  ", turns, "guesses left")
-            else:
-                counter -=word.count(newGuess) #deleten repeated letters
-                print("nice guess!")
-            guesses += newGuess 
-        else:
-            print("you used this one already")
-        updateWord(word, guesses, turns)
+#always have a way to close your screen  
 
-        #end of whole loop with
+word=random.choice(gameWords).upper() 
+print(word) 
+turns= 0   #should we conider controlling this number when he/she misses      
+guesses=[] 
+check = True  
+while check:  
+    for event in pygame.event.get():  
+        if event.type == pygame.QUIT:  
+            check = False     
+        if event.type == pygame.MOUSEBUTTONDOWN: 
+            mx, my =pygame.mouse.get_pos() 
+            for letter in letters: 
+                x,y,ltr,see=letter 
+                if see: 
+                    rect=pygame.Rect(x-Wbox/2, y-Wbox/2,Wbox, Wbox) 
+                    if rect.collidepoint(mx,my):  #if letter has been click 
+                        letter[3]=False 
+                        guesses.append(ltr) 
+                        if ltr not in word: 
+                            turns +=1 
+    displayWord=updateWord(word, guesses)  
+    updateScreen(turns, displayWord)  
+    #check if we won or lost the game 
+    
+    won=True 
+    for letter in word: 
+        if letter not in guesses: 
+            won=False 
+            break 
+    if won: 
+        dis_message("You Won!!!")
+        cont() 
+        break
 
-pygame.quit()
-sys.exit
+    if turns == 6: 
+        dis_message("You lost") 
+        cont()
+
+pygame.quit()  
+sys.exit()  
+
+ 
